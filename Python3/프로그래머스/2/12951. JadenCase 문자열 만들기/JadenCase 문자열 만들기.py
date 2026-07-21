7badad8808107bda9bbab8161839e067054fd137
1jelly7 <97420187+1jelly7@users.noreply.github.com>
2026-07-21 14:33:45 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,9 @@
+def solution(s):    
+    answer = []
+    for i, ch in enumerate(s):
+        if i == 0 or s[i - 1] == ' ':
+            answer.append(ch.upper())
+        else:
+            answer.append(ch.lower())
+    
+    return ''.join(answer)
