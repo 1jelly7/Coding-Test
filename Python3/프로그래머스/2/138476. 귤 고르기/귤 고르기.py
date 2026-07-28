058df1b0ecf6b084b5c8bed6c7177b81e346dd3d
1jelly7 <97420187+1jelly7@users.noreply.github.com>
2026-07-28 11:16:42 +0900

    [level 2] Title: 귤 고르기, Time: 14.96 ms, Memory: 23.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ Python3/프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,29 +1,20 @@
+from collections import Counter
+
 def solution(k, tangerine):
-    size_set = set(tangerine)
-    count_dict = {}
-    for size in size_set:
-        count_dict[size] = 0
+    # Count how many tangerines exist for each size
+    size_counts = Counter(tangerine)
+    
+    # Sort sizes by frequency in descending order.
+    counts = sorted(size_counts.values(), reverse=True)
+    
+    # Pick the most frequent sizes until we have at least k tangerines.
+    answer, picked = 0, 0
+    for count in counts:
+        picked += count
+        answer += 1
         
-    for size in tangerine:
-        count_dict[size] += 1
+        # Stop as soon as we can collect at least k tangerines.
+        if picked >= k:
+            break
     
-    count_list = sorted(count_dict.values(), reverse=True)
-    
-    """sorted_list = sorted(tangerine)
-    
-    count_list = []
-    start, current_size = 0, sorted_list[0]
-    for end in range(1, len(sorted_list)):
-        if current_size != sorted_list[end]:
-            count_list.append(end - start)
-            start = end
-            current_size = sorted_list[end]
-    count_list.append(end - start + 1)
-    count_list.sort(reverse=True)"""
-    
-    count_sum = 0
-    for i, count in enumerate(count_list, start=1):
-        count_sum += count
-        if count_sum >= k:
-            return i
-    
+    return answer
