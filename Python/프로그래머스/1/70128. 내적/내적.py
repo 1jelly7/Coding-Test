72b336b31732c7e49a2ee3b6c78965a42257f397
1jelly7 <97420187+1jelly7@users.noreply.github.com>
2026-08-08 16:58:14 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 11.5 MB -BaekjoonHub

--- Python/프로그래머스/1/70128. 내적/내적.py
+++ Python/프로그래머스/1/70128. 내적/내적.py
@@ -1,2 +1,11 @@
-def solution(a, b):
+def solution(a, b):    
+    try:
+        iter(a)
+        iter(b)
+    except TypeError:
+        raise TypeError("Both a and b must be iterable")
+    
+    if len(a) != len(b):
+        raise ValueError("a and b must have the same length")
+    
     return sum(x * y for x, y in zip(a, b))
