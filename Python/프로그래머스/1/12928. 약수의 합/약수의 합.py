8f6eb4e317ca63d39da2b4e687f41c86d6de932b
1jelly7 <97420187+1jelly7@users.noreply.github.com>
2026-08-08 14:54:47 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 11.5 MB -BaekjoonHub

--- Python/프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ Python/프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,2 +1,4 @@
 def solution(n):
-    return sum(i for i in range(1, n + 1) if n % i == 0)
+    # Sum all proper divisors of n (numbers from 1 up to n//2 that divide n).
+    # Then add n itself to get the sum of all divisors including n.
+    return n + sum(i for i in range(1, n // 2 + 1) if n % i == 0)
