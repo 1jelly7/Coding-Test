e32f1ec69c2f31c743ea7e7a8bb03b36a0fbbac1
1jelly7 <97420187+1jelly7@users.noreply.github.com>
2026-08-08 16:39:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 11.6 MB -BaekjoonHub

--- Python/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ Python/프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -3,10 +3,7 @@ def solution(num):
     max_steps = 500
     
     while num != 1:        
-        if num & 1:
-            num = num * 3 + 1
-        else:
-            num //= 2
+        num = 3 * num + 1 if num % 2 else num // 2
         
         steps += 1
         
