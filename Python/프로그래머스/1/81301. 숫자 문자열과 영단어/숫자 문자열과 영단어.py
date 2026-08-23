54a6022b75e2d127ab542cdeaced2a4b4b1ad5d7
1jelly7 <97420187+1jelly7@users.noreply.github.com>
2026-08-23 20:22:02 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- Python/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ Python/프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,35 +1,18 @@
 def solution(s):
-    number_words = {
-        "zero": "0",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    }
+    number_words = (
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    )
     
-    digits = []
-    current_word = []
-    
-    for ch in s:
-        if ch.isdigit():
-            digits.append(ch)
-            continue
-        
-        current_word.append(ch)
-        
-        candidate_word = "".join(current_word)
-        if candidate_word in number_words:
-            digits.append(number_words[candidate_word])
-            current_word.clear()
-    
-    return int("".join(digits))
-    
-    # for k, v in number_words.items():
-    #     s = s.replace(k, v)
+    for digit, word in enumerate(number_words):
+        s = s.replace(word, str(digit))
     
     return int(s)
