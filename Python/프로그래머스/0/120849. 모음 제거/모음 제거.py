90396e6878c16da906a738c02ef0ca03a4f8703b
1jelly7 <97420187+1jelly7@users.noreply.github.com>
2026-09-16 13:48:50 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- Python/프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ Python/프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,6 +1,6 @@
 def solution(my_string):
-    vowels = ("a", "e", "i", "o", "u")
+    vowels = {"a", "e", "i", "o", "u"}
     
-    result = [s for s in my_string if s not in vowels]
+    result = [ch for ch in my_string if ch not in vowels]
     
     return "".join(result)
